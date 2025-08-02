449c7437e5cf7e868588d64927754e74cd92a50a
mahesabagusr <mahesabagusraditya1@gmail.com>
2025-08-02 23:59:09 +0700

    fix : prisma schema for deletion of provinces and regencies

--- prisma/migrations/20250802165033_added_cascade_delete_to_regencies/migration.sql
+++ prisma/migrations/20250802165033_added_cascade_delete_to_regencies/migration.sql
@@ -1,17 +1,17 @@
 -- DropForeignKey
-ALTER TABLE `address` DROP FOREIGN KEY `Address_province_id_fkey`;
+ALTER TABLE `Address` DROP FOREIGN KEY `Address_province_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `address` DROP FOREIGN KEY `Address_regency_id_fkey`;
+ALTER TABLE `Address` DROP FOREIGN KEY `Address_regency_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `address` DROP FOREIGN KEY `Address_report_id_fkey`;
+ALTER TABLE `Address` DROP FOREIGN KEY `Address_report_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `address` DROP FOREIGN KEY `Address_user_id_fkey`;
+ALTER TABLE `Address` DROP FOREIGN KEY `Address_user_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `regencies` DROP FOREIGN KEY `Regencies_province_id_fkey`;
+ALTER TABLE `Regencies` DROP FOREIGN KEY `Regencies_province_id_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `Address` ADD CONSTRAINT `Address_province_id_fkey` FOREIGN KEY (`province_id`) REFERENCES `Provinces`(`province_id`) ON DELETE CASCADE ON UPDATE CASCADE;
