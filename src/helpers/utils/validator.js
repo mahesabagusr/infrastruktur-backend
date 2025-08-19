0af81a4119c3b2eeb12f1e9cd1a6a5fcbc7e9b5b
Mahesa Bagus Raditya <mahesabagusraditya1@gmail.com>
2025-08-19 17:45:07 +0700

    feat : add search reports with username & signature

--- src/helpers/utils/validator.js
+++ src/helpers/utils/validator.js
@@ -3,7 +3,11 @@ import ExpectationFailed from '@/helpers/error/expectation_failed_error.js';
 import * as wrapper from '@/helpers/utils/wrapper.js';
 
 export const isValidPayload = (payload, model) => {
-  const { value, error } = model.validate(payload);
+  const { value, error } = model.validate(payload, {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true,
+  });
 
   if (error) {
     return wrapper.error(new ExpectationFailed(error.message));
