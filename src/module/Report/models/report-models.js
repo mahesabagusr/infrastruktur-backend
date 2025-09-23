909da9c7c351e2c2ba50d2be4aed9abf521ea6b4
xeron <kaqyword2304@gmail.com>
2025-09-23 13:05:37 +0700

    resolce conflict report-model

--- src/module/Report/models/report-models.js
+++ src/module/Report/models/report-models.js
@@ -115,15 +115,6 @@ const getAllReportModel = joi.object({
     'number.base': 'province ID harus berupa angka.',
     'number.integer': 'province ID harus berupa bilangan bulat.',
     'number.positive': 'province ID harus berupa angka positif.',
-  provinceId: joi.number().integer().positive().optional().messages({
-    'number.base': 'Provinsi ID harus berupa angka.',
-    'number.integer': 'Provinsi ID harus berupa bilangan bulat.',
-    'number.positive': 'Provinsi ID harus berupa angka positif.',
-  }),
-  regencyId: joi.number().integer().positive().optional().messages({
-    'number.base': 'Kabupaten/Kota ID harus berupa angka.',
-    'number.integer': 'Kabupaten/Kota ID harus berupa bilangan bulat.',
-    'number.positive': 'Kabupaten/Kota ID harus berupa angka positif.',
   }),
 });
 
